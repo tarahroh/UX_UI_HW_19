80fcfff397f8b19558d5c4f88f3d01ee104c93c5
tarahroh <77488372+tarahroh@users.noreply.github.com>
2021-10-09 12:42:57 +1100

    Tab states using css complete

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Your Index.js is loaded correctly")
+
+//Test
+
+$('.UxNavBar').on('click', function () {
+    $(this).addClass('activated'); //eventually removeClass of some previous class
+    //other stuff
+});
