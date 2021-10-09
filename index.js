58f9ec1791a3e230f101d9c7fb8d4a5eaa7d6171
tarahroh <77488372+tarahroh@users.noreply.github.com>
2021-10-09 19:30:32 +1100

    modal created
    Javascript still not behaving. Again turned off to work on the last interactive element.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 console.log("Your Index.js is loaded correctly")
 
 //loader
-$(window).on('load', function () {
-    $(".loader").fadeOut(1000);
-    $(".wholepage").fadein(1000);
-});
+//$(window).on('load', function () {
+// $(".loader").fadeOut(1000);
+// $(".wholepage").fadein(1000);
+//});
+
+//modal
+
+// var resumeButton = document.querySelector("#resumeButton:hover");
+// var modal_background = document.querySelector(".modal_background");
+
+// $(resumeButton).on("click", Function(){ $(".modal_background").attr("background_active") });
