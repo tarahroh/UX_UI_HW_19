3a15e127d9e4e972aa460b82be7d12828d8902fa
tarahroh <77488372+tarahroh@users.noreply.github.com>
2021-10-09 14:34:19 +1100

    Header complete
    Logo links to home page. Hover state and active state attached.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
+//test
 console.log("Your Index.js is loaded correctly")
 
-//Test
-
-$('.UxNavBar').on('click', function () {
-    $(this).addClass('activated'); //eventually removeClass of some previous class
-    //other stuff
-});
