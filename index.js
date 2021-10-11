cfbf349785089451dacc0da56b7093cc9aa8ea91
tarahroh <77488372+tarahroh@users.noreply.github.com>
2021-10-11 18:39:18 +1100

    JQuery
    The basic level has been attached to the website.

--- index.js
+++ index.js
@@ -2,14 +2,29 @@
 console.log("Your Index.js is loaded correctly")
 
 //loader
-//$(window).on('load', function () {
-// $(".loader").fadeOut(1000);
-// $(".wholepage").fadein(1000);
-//});
+$(window).on("load", function () {
+  $(".wholepage").hide();
+  function fade() {
+    $(".loader").fadeOut("slow");
+  }
+  setTimeout(fade, 90000);
+  $(".wholepage").show();
+});
 
-//modal
-
-// var resumeButton = document.querySelector("#resumeButton:hover");
-// var modal_background = document.querySelector(".modal_background");
-
-// $(resumeButton).on("click", Function(){ $(".modal_background").attr("background_active") });
+//modalopen
+$(".modal_background").hide();
+if ($(this).on("click", function () {
+  $(".modal_background").show();
+}));
+//modalclose
+$(".modal").clickOutsideThisElement(function () {
+});
+//skills examples open
+$("#A").on("click", function () { $(".XD").slideToggle(); });
+$("#B").on("click", function () { $(".figma").slideToggle(); });
+$("#C").on("click", function () { $(".iL").slideToggle(); });
+$("#D").on("click", function () { $(".Ps").slideToggle(); });
+$("#E").on("click", function () { $(".Pr").slideToggle(); });
+$("#F").on("click", function () { $(".Ae").slideToggle(); });
+$("#G").on("click", function () { $(".SolidWorks").slideToggle(); });
+$("#H").on("click", function () { $(".fusion").slideToggle(); });
